4221d653a840070b4a139ef1c11eff3fa8aaca02
Parthib24 <sirajammunirparthi24@gmail.com>
2018-01-05 08:02:43 +0000

    wiko6580: first commit
     Upload from wikogeek.com/LENNY3 MAX_OpenSource.zip
    
     Devices(20):
     * p4200
     * p5000
     * s5030
     * s5250
     * tinno6580_we_m
     * tinno6580_weg_m
     * v2500
     * v2500_512
     * v2504
     * v2504_512
     * v2600
     * v2600_512
     * v2800
     * v2800_512
     * v2802
     * v3610
     * v3702
     * v3750
     * v3800
     * v3802
    
     Linux kernel 3.18.19
     Supported SDK(s) = 23
    
    Signed-off-by: Parthib24 <sirajammunirparthi24@gmail.com>

---
+++ drivers/misc/mediatek/mach/mt6580/v3802/touchpanel/ft6336_v3800/ftbin_hrc.h
@@ -0,0 +1,7 @@
+static const u8 ftbin_HRC[]= 
+{
+
+#include "HRC_FT6x36_3800_Ver0x13_20160929_app.i"
+
+};
+
---
+++ drivers/misc/mediatek/mach/mt6580/v3802/touchpanel/ft6336_v3800/ftbin_yeji.h
@@ -0,0 +1,7 @@
+static const u8 ftbin_yeji[]= 
+{
+
+#include "YEJI_FT6336U_3800_Ver0x1a_20161121_app.i"
+
+};
+
